2c75efdc7108c4b78312deb29ff2494150285c39
Bibin30 <bibinjoseraj@gmail.com>
2024-11-09 19:35:30 +0530

    Code pushed

---
+++ src/org/add/GreensTech.java
@@ -0,0 +1,37 @@
+package org.add;
+
+public class GreensTech {
+	private void greensOmr() {
+		// TODO Auto-generated method stub
+		System.out.println("Greens OMR");
+
+	}
+	private void greensAdayar() {
+		// TODO Auto-generated method stub
+		System.out.println("Greens Adayar");
+
+	}
+	private void greensTambaram() {
+		// TODO Auto-generated method stub
+		System.out.println("Greens Tambaram Sanatorium");
+
+	}
+	private void greensVelacherry() {
+		// TODO Auto-generated method stub
+		System.out.println("Greens Velacherry");
+	}
+	private void greensAnnaNagar() {
+		// TODO Auto-generated method stub
+		System.out.println("Greens Anna Nagar");
+
+	}
+	public static void main(String[] args) {
+		GreensTech grt=new GreensTech();
+		grt.greensOmr();
+		grt.greensTambaram();
+		grt.greensAdayar();
+		grt.greensVelacherry();
+		grt.greensAnnaNagar();
+	}
+
+}
